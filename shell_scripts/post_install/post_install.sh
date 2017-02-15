c74e6ab40e38b0d6ad5f110ff37ea606fbaf691d
00riddle00 <tomasgiedraitis@gmail.com>
2017-02-15 11:25:27 +0200

    edit post instal lscript

--- shell_scripts/post_install/post_install.sh
+++ shell_scripts/post_install/post_install.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 
-#git clone https://github.com/00riddle00/dotfiles.git $DOTFILES_DIR &&
+export DOTFILES_DIR=$HOME/.riddle/.dotfiles
+export SHELL_SCRIPTS_DIR=$HOME/.riddle/.shell-scripts
+export AUTOMATION_DIR=$HOME/.riddle/.automation
+export HOME_DIR=$HOME/.riddle
+
+
+git clone https://github.com/00riddle00/dotfiles.git $DOTFILES_DIR &&
 
 echo "### Fetched dotfiles ###"
 
-#git clone https://github.com/00riddle00/shell-scripts.git $SHELL_SCRIPTS_DIR &&
+git clone https://github.com/00riddle00/shell-scripts.git $SHELL_SCRIPTS_DIR &&
 
 echo "### Fetched shell scripts ###"
 
-#git clone https://github.com/00riddle00/automation-scripts.git $AUTOMATION_DIR &&
+git clone https://github.com/00riddle00/automation-scripts.git $AUTOMATION_DIR &&
 
 echo "### Fetched automation scripts ###"
 

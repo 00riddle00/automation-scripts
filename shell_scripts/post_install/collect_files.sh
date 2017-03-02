d5dbde2601d70e86d19df0a3a95f44bcd71dcf54
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-03-02 09:23:42 +0200

    rm static files (candy) from scp

--- shell_scripts/post_install/collect_files.sh
+++ shell_scripts/post_install/collect_files.sh
@@ -2,11 +2,11 @@
 
 export HOME_DIR=$HOME/.riddle
 
-zip -r candy.zip $HOME_DIR/.candy &&
+#zip -r candy.zip $HOME_DIR/.candy &&
 zip -r gtd.zip $HOME_DIR/.gtd &&
 zip -r sec.zip $HOME_DIR/.sec &&
 
-scp candy.zip root@tomasgiedraitis.com:/var/www/ && rm -rf candy.zip &&
+#scp candy.zip root@tomasgiedraitis.com:/var/www/ && rm -rf candy.zip &&
 scp gtd.zip root@tomasgiedraitis.com:/var/www/ && rm -rf gtd.zip &&
 scp sec.zip root@tomasgiedraitis.com:/var/www/ && rm -rf sec.zip &&
 
--- shell_scripts/post_install/download_files.sh
+++ shell_scripts/post_install/download_files.sh
@@ -3,11 +3,11 @@
 export HOME_DIR=$HOME/.riddle
 
 #wget -nd -r --no-parent http://www.tomasgiedraitis.com:8010/static/candy.zip &&
-scp root@tomasgiedraitis.com:/var/www/candy.zip candy.zip && 
+#scp root@tomasgiedraitis.com:/var/www/candy.zip candy.zip && 
 scp root@tomasgiedraitis.com:/var/www/gtd.zip gtd.zip && 
 scp root@tomasgiedraitis.com:/var/www/sec.zip sec.zip && 
 
-unzip candy.zip -d $HOME_DIR && rm -rf candy.zip &&
+#unzip candy.zip -d $HOME_DIR && rm -rf candy.zip &&
 unzip gtd.zip -d $HOME_DIR && rm -rf gtd.zip &&
 unzip sec.zip -d $HOME_DIR && rm -rf sec.zip &&
 
